87ac5105aaadb13e28ca0ca5360f0ad446b5eae4
lmarsden <lmarsden@deloitte.co.uk>
2021-05-01 18:48:01 +0100

    Added initial project setup

---
+++ src/main/java/com/lmarsden/controller/BaseController.java
@@ -0,0 +1,15 @@
+package com.lmarsden.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+    @GetMapping("/")
+    public String index() {
+        return "The current time is " + LocalDateTime.now();
+    }
+}
---
+++ src/test/java/com/lmarsden/BaseControllerTest.java
@@ -0,0 +1,4 @@
+package com.lmarsden;
+
+public class BaseControllerTest {
+}
