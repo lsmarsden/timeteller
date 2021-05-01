170582f0d10401c321c034da040c480597b39aa6
lmarsden <lmarsden@deloitte.co.uk>
2021-05-01 22:42:42 +0100

    Added dockerfile, pom, unit tests, jenkinsfile

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM adoptopenjdk/openjdk8:alpine-slim
+
+COPY target/timeteller-0.0.1-SNAPSHOT.jar timeteller-0.0.1-SNAPSHOT.jar
+
+EXPOSE 8010
+
+CMD "java -jar timeteller-0.0.1-SNAPSHOT.jar"
--- src/test/java/com/lmarsden/BaseControllerTest.java
+++ src/test/java/com/lmarsden/BaseControllerTest.java
@@ -1,4 +1,25 @@
 package com.lmarsden;
 
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.lmarsden.controller.BaseController;
+
+@WebMvcTest(BaseController.class)
 public class BaseControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testGetIndexOkResponse() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(containsString("The current time is")));
+    }
 }
